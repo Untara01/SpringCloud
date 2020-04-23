07edb25d1ff293a96af048c4334bfa8faf6cad9b
UncleXu <yzucattree@163.com>
2020-04-23 11:19:10 +0800

    测试

--- ticket-consumer-eureka/src/main/java/com/simons/cn/TicketConsumerEurekaApplication.java
+++ ticket-consumer-eureka/src/main/java/com/simons/cn/TicketConsumerEurekaApplication.java
@@ -7,8 +7,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient  //该注解标识这个一个该服务包含了Eureka Client
 public class TicketConsumerEurekaApplication {
-
+    //尝试commit
     public static void main(String[] args) {
+
+        int i=0;
         SpringApplication.run(TicketConsumerEurekaApplication.class,args);
     }
 
